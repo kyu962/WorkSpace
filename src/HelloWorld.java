f94d7818cadec32da8dd9baf727b84330dd655dc
jklee <jklee@SDSG1002904.samsung.samsungeurope.de>
2021-02-25 18:20:39 +0100

    adding new line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] orgs) {
 		System.out.println("Hello World");
 		System.out.println("I am JK");
+		System.out.println("New learning Java");
 	}
 
 }
